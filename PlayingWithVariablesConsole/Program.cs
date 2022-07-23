2319aeca16d901b7f4431120709508d8a59e479a
Andrew Bayer <andrewbayer21@gmail.com>
2022-07-23 18:28:34 -0400

    initial commit

---
+++ PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿// Variable Declarations
+int age;
+string name;
+bool amICorrect;
+char firstLetter;
+char secondLetter;
+decimal number;
+double anotherNumber;
+
+// Variable inializations
+
+age = 47;
+name = "John";
+amICorrect = false;
+firstLetter = 'F';
+secondLetter = 'H';
+number = 0.72m;
+anotherNumber = 1000.23;
+
+    Console.WriteLine($"My name is {name} and I am {age} years old. My initials are {firstLetter}{secondLetter}. My savings account reads {number} because my electric bill is {anotherNumber}. Am I telling the truth? {amICorrect}.");
+
+
+
